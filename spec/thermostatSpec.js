e4dc4f655698576b725723fd72d0b2f5e1c4cd90
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-06-12 14:59:14 +0100

    min and max temps, powersave default to on

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,4 +19,25 @@ describe("Thermostat", function() {
     expect(thermostat._temperature).toEqual(15)
   });
 
+  it("won't let the user decrease beyond 10 degrees", function() {
+    thermostat.decrease(10)
+    thermostat.decrease(1)
+    expect(thermostat._temperature).toEqual(10)
+  })
+
+  it("won't let the user increase beyond 25 degrees by default", function() {
+    thermostat.increase(5)
+    thermostat.increase(1)
+    expect(thermostat._temperature).toEqual(25)
+  })
+
+  it("is in powersave mode by default", function() {
+    expect(thermostat.powerSave()).toEqual(true)
+  })
+
+  xit("has a max temperature of 32 degrees when power save mode is off", function() {
+    thermostat.togglePowerSave()
+    thermostat.increase()
+  })
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,16 @@
 function Thermostat() {
   this._temperature = 20;
+  this._powerSave = true
 }
 
 Thermostat.prototype.increase = function(up) {
-  this._temperature += up;
+  (this._temperature + up <= 25) ? this._temperature += up : this._temperature = 25
 }
 
 Thermostat.prototype.decrease = function(down) {
-  this._temperature -= down;
+  (this._temperature - down >= 10) ? this._temperature -= down : this._temperature = 10
+}
+
+Thermostat.prototype.powerSave = function() {
+  return this._powerSave
 }
